4ea719e3925f23d718abd60e33c061ce2ee48983
walterDante2022 <112218068+walterDante2022@users.noreply.github.com>
2024-09-08 21:30:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import StandardScaler
 
 # Cargar el modelo entrenado
 with open('modelo_optimizado.pkl', 'rb') as file:
-    modelo = pickle.load(file)
+    modelo = pickle.load('file')
 
 # Definir la interfaz de usuario en Streamlit
 st.title('Predicción de Precios de Laptops')
@@ -15,6 +15,7 @@ st.title('Predicción de Precios de Laptops')
 ram = st.number_input('RAM (GB)', min_value=1, max_value=64, value=8)
 screen_width = st.number_input('Ancho de Pantalla', min_value=800, max_value=4000, value=1920)
 screen_height = st.number_input('Alto de Pantalla', min_value=600, max_value=3000, value=1080)
+ssd = st.number_input('SSD (GB)', min_value=0, max_value=2000, value=256)
 ghz = st.number_input('GHz del CPU', min_value=0.1, max_value=5.0, value=2.5)
 type_gaming = st.selectbox('¿Es Gaming?', ['No', 'Sí'])
 type_notebook = st.selectbox('¿Es Notebook?', ['No', 'Sí'])
@@ -26,8 +27,8 @@ type_notebook = 1 if type_notebook == 'Sí' else 0
 # Botón para realizar predicción
 if st.button('Predecir Precio'):
     # Crear DataFrame con las entradas
-    input_data = pd.DataFrame([[ghz , screen_width, screen_height , ram ]],
-                    columns=['Ghz', 'screen_width', 'screen_height', 'Ram'])
+    input_data = pd.DataFrame([[ram, screen_width, screen_height, ssd, ghz, type_gaming, type_notebook]],
+                    columns=['Ram', 'screen_width', 'screen_height', 'SSD', 'GHz', 'TypeName_Gaming', 'TypeName_Notebook'])
 
     # Estandarización de las características
     scaler = StandardScaler()
